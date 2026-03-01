f1f5c3ed448725afe4a0fcddea8cdb2729dc7320
ppp3ppj <ppp.3ppj@gmail.com>
2026-03-01 17:09:18 +0700

    Add default sysadmin creation logic in seeds script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Ditto.Accounts.User
+alias Ditto.Repo
+
+# Default sysadmin credentials:
+#   Email:    admin@admin.com
+#   Password: Admin@123456
+#
+# Change the password immediately after first login via /users/settings.
+
+sysadmin_attrs = %{
+  "email" => "admin@admin.com",
+  "username" => "sysadmin",
+  "password" => "Admin@123456"
+}
+
+case Repo.get_by(User, email: "admin@admin.com") do
+  nil ->
+    %User{}
+    |> User.registration_changeset(sysadmin_attrs)
+    |> Ecto.Changeset.put_change(:is_sysadmin, true)
+    |> Repo.insert!()
+
+    IO.puts("✓ Default sysadmin created: admin@admin.com / Admin@123456")
+
+  _existing ->
+    IO.puts("- Sysadmin admin@admin.com already exists, skipping.")
+end
