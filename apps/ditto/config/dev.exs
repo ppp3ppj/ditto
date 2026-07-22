232132c13e51f1dc473e7d77ea07b4d352d7ccbf
ppp3ppj <ppp.3ppj@gmail.com>
2026-07-22 09:44:32 +0700

    Add 0.0.0.0 when develop at wsl block

--- apps/ditto/config/dev.exs
+++ apps/ditto/config/dev.exs
@@ -16,7 +16,9 @@ config :ditto, Ditto.Repo,
 config :ditto, DittoWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  # WHEN I RUN ON WSL I GOT BLOCK 127.0.0.1
+  # http: [ip: {127, 0, 0, 1}],
+  http: [ip: {0, 0, 0, 0}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
